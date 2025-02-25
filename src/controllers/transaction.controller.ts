1bae93e5cbcc7ffebbdde6d3c678e1e44745ebc2
delighted <deborahdelighted5@gmail.com>
2025-02-25 23:19:07 +0100

    wip:added controller to get user transaction history

--- src/controllers/transaction.controller.ts
+++ src/controllers/transaction.controller.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from 'express';
 import {  transaction_service,  } from '../models/transaction.model';
 // import { update_user_balance } from '../services/user.service';
-import { deposit_money_validator, transfer_funds_validator, validate_transfer_validator } from '../validators/transaction.validator';
+import { deposit_money_validator, get_user_transaction_history_validator, transfer_funds_validator, validate_transfer_validator } from '../validators/transaction.validator';
 import { user_service } from '../models/user.model';
 import { TransactionStatus, TransactionType, User } from '@prisma/client';
 import { verifyPassword } from '../utils/util';
 import { func } from 'joi';
 import { basename } from 'path';
+import { io } from '../utils/socket.util';
 
 
 export const initialize_payment = async (req: Request, res: Response) => {
@@ -115,6 +116,7 @@ export const transfer_fund = async (req: Request, res: Response) => {
     const receiver_new_balance = result.data.receiver.balance + value.amount
     const updated_receiver = await user_service.update_user_balance(value.receiver_id, receiver_new_balance);
 
+    send_notification(result.data.sender,result.data.receiver,value.amount)
 
     await transaction_service.create_transaction(data,TransactionStatus.SUCCESSFULL)      
 
@@ -190,4 +192,51 @@ async function validate_values(amount:number,sender_id:string,receiver_id:string
     }
 
 
+}
+
+export const get_user_transaction_history = async (req: Request, res: Response) => {
+  try {
+    const { error, value } = get_user_transaction_history_validator(req.body);
+
+    if(error){
+        throw new Error(error );
+    }
+    const found_user = await user_service.get_account_by_id(value.user_id)
+    
+    if(!found_user){
+        throw new Error("User Doesn't Exist")
+    }
+    const isValidPassword = await verifyPassword(value.password, found_user.password);
+    
+    if (!isValidPassword){
+      throw new Error("Invalid details")
+    }
+
+    const transactions = await transaction_service.get_user_transactions(value.user_id)
+    
+    res.status(200).send({
+      status: 'success',
+      message: 'Payment initialized successfully',
+      data: transactions,
+    });
+  } catch (error: any) {
+     res.status(500).send({
+      status: 'failed',
+      message: error.message,
+    });
+  }
+};
+
+async function send_notification(sender:User,receiver:User,amount:number){
+  io.emit('notification', {
+    userId: sender.id,
+    message: `You have transferred ₦${amount} to  ${receiver.last_name + receiver.first_name}.`,
+    balance: sender.balance,
+  });
+
+  io.emit('notification', {
+    userId: receiver.id,
+    message: `You have received ₦${amount} from user ${sender.last_name + receiver.first_name}.`,
+    balance: receiver.balance,
+  });
 }
