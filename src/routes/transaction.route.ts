4b97ab249f35193c57ea369d6213576138999a77
delighted <deborahdelighted5@gmail.com>
2025-02-25 23:22:21 +0100

    wip: added the routes to get transaction history, and autentication to all routes

--- src/routes/transaction.route.ts
+++ src/routes/transaction.route.ts
@@ -1,16 +1,21 @@
 import express from "express";
-import {  initialize_payment, transfer_fund, verify_transaction } from "../controllers/transaction.controller";
+import {  get_user_transaction_history, initialize_payment, transfer_fund, verify_transaction } from "../controllers/transaction.controller";
+import { authenticate } from "../utils/util";
 
 const routes = express.Router();
 
 export const PATHS = {
     initialize_payment : "/initialize-payment",
     verify_transaction:"/verify-transaction",
-    transfer_fund:"/transfer-fund"
+    transfer_fund:"/transfer-fund",
+    get_user_transaction_history:"/get-user-transaction-history"
 
 };
 
-routes.post(PATHS.initialize_payment,initialize_payment)
-routes.post(PATHS.verify_transaction,verify_transaction)
-routes.post(PATHS.transfer_fund,transfer_fund)
+routes.post(PATHS.initialize_payment,authenticate,initialize_payment)
+routes.post(PATHS.verify_transaction,authenticate,verify_transaction)
+routes.post(PATHS.transfer_fund,authenticate,transfer_fund)
+routes.get(PATHS.get_user_transaction_history,authenticate,get_user_transaction_history)
+
+
 export default routes;
